8571f048d5170d8b38abf2efcec1806d665eae44
Nouni22 <aurorenouni97@gmail.com>
2026-04-08 20:30:22 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+ #print("J'apprends Python!")
+
+#print(17 + 35 * 2) 
+
+#nom = "NOUNI"
+#prenom = "Aurore"
+#age = 28
+#print(f"Je m'appelle {prenom} et j'ai {age} ans")
+#age = age + 10
+#print(f"Je m'appelle {prenom} et j'ai {age} ans maintenant")
+
+
+import pandas as pd
+import numpy as np
+
+print("Projet actuariat lancé 🚀")
+
+df = pd.DataFrame({
+    "age": [25, 40, 60],
+    "prime": [10000, 20000, 15000]
+})
+
+print(df)
